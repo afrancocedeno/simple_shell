ae7f6e1af174f84db57ac527df814d6ad05d3f3b
vagava <2380@holbertonschool.com>
2020-11-26 04:12:06 +0000

    00 | file create

---
+++ envpath.c
@@ -0,0 +1,9 @@
+int main(void)
+{
+/* identificar PATH en doble puntero **env */
+/* tokenizar con "=" PATH = __rutas__ */
+/* identificar segundo token */
+/* tokenizar string resultante con : separando rutas */
+/* identificar la ruta solicitada */
+return (0);
+}
